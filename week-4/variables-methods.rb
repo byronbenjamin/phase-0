5d3714054e06de265905ce6306e8d8dd84fcd3dd
byronbenjamin <byrongage@yahoo.com>
2015-12-28 14:26:26 -0600

    Add Week 4 Assignments

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,63 @@
+#Full name greeting:
+
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "What is you middle name?"
+
+middle_name = gets.chomp
+
+puts "What is you last name?"
+
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{middle_name} #{last_name}!"
+
+
+#Bigger, better favorite number:
+
+puts "What is your favorite number?"
+
+number = gets.to_i + 1
+
+puts "#{number} is a bigger and better number"
+
+
+
+=begin
+
+How do you define a local variable?
+
+You define a local variable inside of a method. local variables are just like global variables but are called upon inside of a method. They should be defined first by giving the local variable a name. The name should be lowercase. Then the "=" symbol and lastly, what the variable will contain.
+
+How do you define a method?
+
+You define a method by first typing "def".
+Next is the methods name. The methods name should be lower case.
+on the next line you write your code for what the method should do and lastly you type "end" to close the method.
+
+What is the difference between a local variable and a method?
+
+A local variable is a variable that is declared within a method. A method uses code and sometimes local variables to output some kind of result.
+
+How do you run a ruby program from the command line?
+
+You have to go to the directory that the ruby file is stored in. From there you type in: ruby ruby_file_goes_here.rb
+
+How do you run an RSpec file from the command line?
+
+You type in the following: respec ruby_file_goes_here.rb
+
+What was confusing about this material? What made sense?
+
+There was nothing too confusing about this material. Most of it was a review. The most confusing thing is just remembering the different methods that is available.
+
+Creating variables and methods made since. Using the ruby methods made sense as well.
+
+=end
+
+[Return A Formatted Address](https://github.com/byronbenjamin/phase-0/blob/master/week-4/address/my_solution.rb
+)
+[Defining Math Methods](https://github.com/byronbenjamin/phase-0/blob/master/week-4/math/my_solution.rb
+)
