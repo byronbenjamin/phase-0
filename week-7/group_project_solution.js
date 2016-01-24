f0a59cd04d08d0cdaf8272fdd46026bef046d7d0
byronbenjamin <byrongage@yahoo.com>
2016-01-24 13:03:32 -0600

    group_solution

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,36 @@
+
+
+function sum(array){
+  var total = 0;
+  for (var i = 0; i < array.length; i++) {
+    total += ary[i];
+    return total;
+  }
+}
+
+// As a user I want to enter the number of students of each grade level so I can have a total number of students in the school. I entered the total number of students in the using sum([23,20,14,25,25]) and received a total number of 107 students.
+
+function mean(array){
+  // sum();
+  var count = array.length;
+  return count;
+  sum ();
+  var result = sum()/count;
+  return result;
+}
+
+// As a user I want to enter the total number of students and recieve a average number of students per classroom.
+
+function median(array){
+    numbers.sort(function(a,b) {
+     return a - b;
+    });
+  var sorted = median();
+  var count = array.length;
+  var middle = sorted/2;
+  return middle;
+}
+
+// As a user I want to add each classes student population and receive an average class size.
+
+
